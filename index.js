c71f7141da78de81d67439ba81ff736640afafbc
PREETSHARMA2507 <148210185+PREETSHARMA2507@users.noreply.github.com>
2024-08-06 22:35:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ for(var i=0;i<num;i++){
        
         switch (buttonInner) {
         case "a":
-            var audi=new Audio("sounds/tom-1.mp3");
+            var audi=new Audio("tom-1.mp3");
             audi.play();
             
             break;
         case "❤️":
-            var lovey=new Audio("sounds/i-love-you-cartoon-voice-136531.mp3");
+            var lovey=new Audio("i-love-you-cartoon-voice-136531.mp3");
             lovey.play();
             
             break;
         case "w":
-            var tom2=new Audio("sounds/tom-2.mp3");
+            var tom2=new Audio("tom-2.mp3");
             tom2.play();
                 
             break;
         case "s":
             
-            var tom3=new Audio("sounds/tom-3.mp3");
+            var tom3=new Audio("tom-3.mp3");
             tom3.play();
                     
             break; 
@@ -32,17 +32,17 @@ for(var i=0;i<num;i++){
                         
              break;
         case "j":
-            var crash=new Audio("sounds/crash.mp3");
+            var crash=new Audio("crash.mp3");
             crash.play();
                             
             break;
         case "l":
-            var kik=new Audio("sounds/kick-bass.mp3");
+            var kik=new Audio("kick-bass.mp3");
             kik.play();
                         
             break;
         case "k":
-            var snare=new Audio("sounds/snare.mp3");
+            var snare=new Audio("snare.mp3");
             snare.play();
                             
             break;
